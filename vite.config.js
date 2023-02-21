e010a6382ab4d072c1040dc0e5cd3d59b294f762
Ali Houssain Sareini <alihsareini@live.com>
2023-02-21 17:31:46 -0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/splash2o-threejs/',
     server:
     {
         host: true,
